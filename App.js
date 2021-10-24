b4ae2f913d9894d6f7b7fad01dfb1e23c096150f
Sam Thorn <spthornnz@gmail.com>
2021-10-24 18:44:25 +1300

    Adds basic UI for writing and adding a to do item

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import colors from "./assets/colors";
 import Todo from "./components/todo";
 
@@ -16,6 +23,19 @@ export default function App() {
           <Todo text={"Task 2"} />
         </View>
       </View>
+
+      {/* Write a Task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeToDoWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"WRITE A TASK"} />
+        <TouchableOpacity>
+          <View style={styles.addToDoWrapper}>
+            <Text style={styles.addButton}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,5 +55,35 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 32,
+  },
+  writeToDoWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    backgroundColor: colors.white,
+    borderRadius: 64,
+    borderColor: colors.silvergrey,
+    borderWidth: 1,
+    width: 250,
+  },
+  addToDoWrapper: {
+    width: 64,
+    height: 64,
+    backgroundColor: colors.white,
+    borderRadius: 64,
+    borderColor: colors.silvergrey,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addButton: {
+    color: colors.silvergrey,
   },
 });
--- assets/colors.js
+++ assets/colors.js
@@ -1,6 +1,7 @@
 const colors = {
   white: "#fff",
   lightgrey: "#E8EAED",
+  silvergrey: "#C0C0C0",
   lightblue: "#55BCF6",
 };
 
